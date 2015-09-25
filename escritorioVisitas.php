e236bd802b95ddcddca4f2f6286fc1ec1f819004
Eze Olea Figueroa <eze.olea.f@gmail.com>
2015-09-25 09:11:39 -0300

    Estilo escritorio

--- escritorioVisitas.php
+++ escritorioVisitas.php
@@ -8,8 +8,14 @@
 <html lang="es">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<script type='text/javascript' src="scripts/jquery-1.11.3.min.js"></script>
-<link rel="stylesheet" href="css/escritorioVisitas.css">
+<!--script type='text/javascript' src="scripts/jquery-1.11.3.min.js"></script>
+<link rel="stylesheet" href="css/escritorioVisitas.css"-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="bootstrap/css/bootstrap-responsive.css">
+<link rel="stylesheet" href="bootstrap/css/estilos.css">
+<script src="bootstrap/js/jquery-1.11.3.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
 <title>Escritorio Visitas</title>
 <script>
 
@@ -25,6 +31,11 @@ $(document).ready(function()
 	if(cantVisitas == 0)
 	{
 		$('#listadoVisitas').hide();
+		$('#noVisitas').show();
+	}
+	else
+	{
+		$('#noVisitas').hide();
 	}
 	//$('#nuevaVisita').accordion({collapsible: true, active: false, header: "h3", icons: false});
 });
@@ -38,64 +49,99 @@ include_once "conexion.php";
 include_once "scripts/libreria.php";
 
 $condicion = 'solicitante_fk='.$_SESSION['id_user'];
-echo $condicion;
 $cantVisitas = contarRegistro('id','visita',$condicion);
 echo "<script>setCantVisitas('".$cantVisitas."')</script>";
 
 ?>
 <body>
-<div id="formulario">
-<h2>Visitas</h2>
-<?php include_once "menuVisitas.html"; ?>
+<div class="container">
+		<div class="margen_sup"></div>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h2 class="panel-title">Listado de visitas</h2>
+			</div>
+		</div>
 
-<div id="tablaCuerpo">
-<a href="registrarVisita.php"><input type="button" name="nuevaVisita" value="Nueva Visita" /></a> <!--Puede ser un icono con un signo mas-->
-<div id="listadoVisitas">
-<h3>Listado de Visitas</h3>
-<table>
-<tr>
-	<td>Nombre</td>
-	<td>Fecha</td>
-	<td>C&aacute;tedra</td>
-	<td>Empresa</td>
-	<td></td>
-</tr>
-<?php
-$condicion = "solicitante_fk=".$_SESSION['id_user'];
-$sqlListadoVisitas = traerSqlCondicion('*','visita',$condicion);
-while($rowListadoVisitas = pg_fetch_array($sqlListadoVisitas))
-{
-	echo '<tr>';
-		echo '<td>';
-			echo $rowListadoVisitas['nombre'];
-		echo '</td>';
-		echo '<td>';
-			echo $rowListadoVisitas['fecha'];
-		echo '</td>';
-		echo '<td>';
-			echo $rowListadoVisitas['catedra'];
-		echo '</td>';
-		echo '<td>';
-			echo $rowListadoVisitas['nombre_empresa'];
-		echo '</td>';
-		echo '<td>';
-			echo '<a href="registrarVisita.php?idVisita='.$rowListadoVisitas['id'].'">Ver m&aacute;s</a>';
-		echo '</td>';
-	echo '</tr>';
-}
+		<ul class="nav nav-pills" style="background-color: #EEEEEE;">
+			<li class="active"><a href="#">Visitas</a></li>
+			<li><a href="registrarVisita.php">Registrar Visita</a></li>
+			<li><a href="login.php">Cerrar Sesi&oacute;n</a></li>
+		</ul>
 
-?>
-</table>
-</div>
-</div>
-</center>
-<table id="tablaBtn" align="center">
-	<tr width="100%">	
-		<td width="100%" align="center">
-			<input id="enviar" class="submit" type="submit" value=""/>
- 		</td>
-	</tr>
-</table>
+		<div class="row" id="listadoVisitas">
+			<div class="col-xs-12">
+				<table class="table table-responsive table-striped">
+					<thead>
+					<tr>
+						<th class="col-xs-3">
+							Nombre
+						</th>
+						<th class="col-xs-2">
+							Fecha
+						</th>
+						<th class="col-xs-4">
+							C&aacute;tedra
+						</th>
+						<th class="col-xs-2">
+							Empresa
+						</th>
+						<th class="col-xs-1">
+							&nbsp;
+						</th>
+					</tr>
+					</thead>
+					<tbody>
+						<?php
+
+						$condicion = "solicitante_fk=".$_SESSION['id_user'];
+						$sqlListadoVisitas = traerSqlCondicion('*','visita',$condicion);
+						while($rowListadoVisitas = pg_fetch_array($sqlListadoVisitas))
+						{
+							$vFecha = explode('-', $rowListadoVisitas['fecha']);
+							$fecha = $vFecha[2].'-'.$vFecha[1].'-'.$vFecha[0];
+							echo '<tr>';
+								echo '<td class="col-xs-3">';
+									echo $rowListadoVisitas['nombre'];
+								echo '</td>';
+								echo '<td class="col-xs-2">';
+									echo $fecha;
+								echo '</td>';
+								echo '<td class="col-xs-4">';
+									echo $rowListadoVisitas['catedra'];
+								echo '</td>';
+								echo '<td class="col-xs-2">';
+									echo $rowListadoVisitas['nombre_empresa'];
+								echo '</td>';
+								echo '<td class="col-xs-1">';
+									echo '<a href="registrarVisita.php?idVisita='.$rowListadoVisitas['id'].'">Ver m&aacute;s</a>';
+								echo '</td>';
+							echo '</tr>';
+						}
+
+						?>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<div class="row" id="noVisitas">
+			<div class="col-xs-12">
+				<table class="table">
+					<thead>
+					<tr>
+						<th class="col-xs-12">
+							Listado de visitas
+						</th>
+					</tr>
+					</thead>
+					<tbody>
+						<td class="col-xs-12">
+							No hay visitas registradas
+						</td>
+					</tbody>
+				</table>
+			</div>
+		</div>
+
 </div>
 </body>
 <?php
