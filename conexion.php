181405056f96972701b8f2900e63d3fbaf10edcb
Eze BIT <efigueroa@bit.com.ar>
2015-11-07 09:15:39 -0300

    Conexion

--- conexion.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conn = pg_connect("host=localhost port=5432 user=postgres password=postgres dbname=visitasDepto") or die("Error de conexion.".pg_last_error());//conexion local
-
-//$conn = pg_connect("host=190.114.198.126 port=5432 user=extension password=newgenius dbname=visitasdepto") or die("Error de conexion.".pg_last_error()); //conexion facu
-?>
